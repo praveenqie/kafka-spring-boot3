80b7d2770c7217ddf44450052113af3404b3635f
praveenqie <praveenkumarks11@gmail.com>
2024-05-26 14:54:02 +0530

    initial commit

---
+++ src/test/java/com/kafka_spring_boot3/KafkaSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka_spring_boot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringBoot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
